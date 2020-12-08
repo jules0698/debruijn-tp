fe9e2a15f86ff6c4c0bd1105f9cc362b6aa5fa4f
jules collat <collatjule@eisti.eu>
2020-12-08 18:12:36 +0100

    poce blo

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -26,7 +26,7 @@ import statistics
 
 __author__ = "Jules Collat"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Jules Collat]
+__credits__ = ["Jules Collat"]
 __license__ = "GPL"
 __version__ = "1.0.0"
 __maintainer__ = "Jules Collat"
@@ -46,7 +46,6 @@ def isfile(path):
         raise argparse.ArgumentTypeError(msg)
     return path
 
-
 def get_arguments():
     """Retrieves the arguments of the program.
       Returns: An object that contains the arguments
@@ -64,26 +63,18 @@ def get_arguments():
                         help="Output contigs in fasta file")
     return parser.parse_args()
 
-
 def read_fastq(fastq_file):
     with open(fastq_file,"rt") as fastqfile:
         for line in fastqfile:
             yield next(fastqfile).replace("\n","")
             next(fastqfile)
             next(fastqfile)
-
-
-    
-
-
+            
 def cut_kmer(read, kmer_size):
     for c, valeur in enumerate(read):
         if len(read[c:kmer_size+c]) == kmer_size:
             yield (read[c:kmer_size+c])
         
-    
-
-
 def build_kmer_dict(fastq_file, kmer_size):
     kmer_dict = dict()
     sequences = read_fastq(fastq_file)
@@ -95,30 +86,33 @@ def build_kmer_dict(fastq_file, kmer_size):
                 kmer_dict[kmer]+=1
     return kmer_dict
     
-
-
-
 def build_graph(kmer_dict):
     tree = nx.DiGraph()
     for kmer, weight in kmer_dict.items():
         tree.add_edge(kmer[0:len(kmer)-1], kmer[1:len(kmer)], weight=weight)
     return tree
 
-    
-
 def remove_paths(graph, path_list, delete_entry_node, delete_sink_node):
     pass
 
 def std(data):
-    pass
-
+    return statistics.stdev(data)
+    
 
 def select_best_path(graph, path_list, path_length, weight_avg_list, 
                      delete_entry_node=False, delete_sink_node=False):
     pass
 
 def path_average_weight(graph, path):
-    pass
+    liste_poids = []
+    for k in range(len(path)-1):
+        noeud1 = path[k]
+        noeud2 = path[k+1]
+        liste_poids.append(graph[noeud1][noeud2]['weight'])
+    return statistics.mean(liste_poids)
+
+
+    
 
 def solve_bubble(graph, ancestor_node, descendant_node):
     pass
@@ -133,16 +127,55 @@ def solve_out_tips(graph, ending_nodes):
     pass
 
 def get_starting_nodes(graph):
-    pass
+    noeud_entree =[]
+    noeuds = graph.nodes()
+    for noeud in noeuds:
+        predecessors=graph.predecessors(noeud) 
+        if len(list(predecessors)) == 0 :
+            noeud_entree.append(noeud)
+    return noeud_entree       
+
+    
 
 def get_sink_nodes(graph):
-    pass
+    noeud_sortie =[]
+    noeuds = graph.nodes()
+    for noeud in noeuds:
+        successors=graph.successors(noeud) 
+        if len(list(successors)) == 0 :
+            noeud_sortie.append(noeud)
+    return noeud_sortie 
+
+    
 
 def get_contigs(graph, starting_nodes, ending_nodes):
-    pass
+    contigs = []
+    for noeud_entree in starting_nodes:
+        for noeud_sortie in ending_nodes:
+            chemin=list(nx.all_simple_paths(graph,noeud_entree,noeud_sortie))
+            for noeud in chemin:
+                chaine=noeud[0]
+                for i in range(1,len(noeud)):
+                    chaine+= noeud[i][-1]
+                tuplee= (chaine,len(chaine))
+                contigs.append(tuplee)
+    return contigs
+                    
+    
+
+
 
 def save_contigs(contigs_list, output_file):
-    pass
+
+    def fill(text, width=80):
+        return os.linesep.join(text[i:i+width] for i in range (0, len(text), width))   
+         
+    with open(output_file,"wt") as outputfile:
+        nombre_contigs = len(contigs_list)
+        for k in range (0,nombre_contigs):
+            outputfile.write(">contig_" + str(k) + " " + "len=" + str(contigs_list[k][1]) + "\n" + fill(contigs_list[k][0], width=80) + "\n")
+    
+
 
 #==============================================================
 # Main program
@@ -155,8 +188,6 @@ def main():
     args = get_arguments()
 
 if __name__ == '__main__':
-    tests = cut_kmer("ATGATAATGGG", 3)
-    for test in tests :
-        print(test)
+    
     
     main()
